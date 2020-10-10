29970cdf915b1234aaa18cc4a11fa1335f6bc054
Grigorev German <ggrigoriev.000spb@gmail.com>
2020-10-10 18:31:19 +0300

    Updates

---
+++ precache-manifest.354ad48f7d2374f35f7caa327f046026.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5123069559e2bd7162e1d2f6508e3c6f",
+    "url": "/lab1/index.html"
+  },
+  {
+    "revision": "3262efb20aa03653695c",
+    "url": "/lab1/static/css/main.27aaaf30.chunk.css"
+  },
+  {
+    "revision": "3262efb20aa03653695c",
+    "url": "/lab1/static/js/main.d6bb5618.chunk.js"
+  },
+  {
+    "revision": "1b8f1790a0d9764551bb",
+    "url": "/lab1/static/js/runtime-main.8159e540.js"
+  }
+]);
