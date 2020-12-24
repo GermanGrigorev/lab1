7ea2fae25ff8a6060e13c79cd07a58d9e07d779b
Grigorev German <ggrigoriev.000spb@gmail.com>
2020-12-24 09:50:07 +0300

    Add loader

--- src/app/renderCurrentWeather/renderCurrentWeather.js
+++ src/app/renderCurrentWeather/renderCurrentWeather.js
@@ -6,12 +6,14 @@ import { setError } from '../error/error';
 import { serverApi } from '../../api/serverApi';
 
 const renderDefaultCurrentWeather = async () => {
+  setIsLoading(true);
   const currentWeather = await serverApi.getWeatherByCityName(DEFAULT_CITY);
   const tag = getCityWeatherNode({ weather: currentWeather, type: CityTypes.current });
   const container = document.getElementById('container');
   const current = container.querySelector('.Current');
   if (current) current.remove();
   container.prepend(tag);
+  setIsLoading(false);
 };
 
 export const renderCurrentWeather = async () => {
